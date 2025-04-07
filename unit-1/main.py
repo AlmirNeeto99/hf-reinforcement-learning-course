1881c47a89f120b46945fb1bcd200c80e1377ad0
Almir Neto <almirneto338@gmail.com>
2025-04-07 10:50:13 -0300

    fix: removing unnecessary imports

--- unit-1/main.py
+++ unit-1/main.py
@@ -1,11 +1,7 @@
 import gymnasium
 
-from huggingface_sb3 import load_from_hub, package_to_hub
-from huggingface_hub import notebook_login
-
 
 from stable_baselines3 import PPO
-from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
